002eb35e29c909b29ac840b3018cf423533a7c5b
El Houssain Souhail <sou.ductive@gmail.com>
2024-01-19 14:02:11 -0800

    Added chek for pip3 and wekzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
 # install Flask v2.1.0  using pip3
 
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip],
 }
