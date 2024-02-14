3cf35e1f797655b0027c8c39061276e0666feb7c
El Houssain Souhail <sou.ductive@gmail.com>
2024-02-14 17:55:05 +0100

    Fixed typo

--- 0x14-mysql/create_replica_user.sql
+++ 0x14-mysql/create_replica_user.sql
@@ -1,6 +1,6 @@
 -- Creates a replica user
 
-CREATE USRE IF NOT EXISTS 'replica_user'@'%'
+CREATE USER IF NOT EXISTS 'replica_user'@'%'
 IDENTIFIED BY 'project280holberton';
 
 GRANT REPLICATION CLIENT, SELECT ON *.* TO 'replica_user'@'%';
