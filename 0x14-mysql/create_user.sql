5b84332a86514509be2284607938ee5f154c9df5
El Houssain Souhail <sou.ductive@gmail.com>
2024-02-14 17:10:25 +0100

    Added script to add MySQL user

---
+++ 0x14-mysql/create_user.sql
@@ -0,0 +1,7 @@
+-- Creates a user
+-- Responsible for checking the configuration of the servers
+
+CREATE USER IF NOT EXISTS 'holberton_user'@'localhost'
+IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT, SELECT *.* TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
