1c297db619ed44e581673df7fcf106aac89697fc
El Houssain Souhail <sou.ductive@gmail.com>
2024-02-14 17:45:23 +0100

    Updated database creating script

--- 0x14-mysql/create_db.sql
+++ 0x14-mysql/create_db.sql
@@ -5,10 +5,12 @@ CREATE DATABASE IF NOT EXISTS tyrell_corp;
 
 CREATE TABLE IF NOT EXISTS tyrell_corp.nexus6
 (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id),
+        id INT UNIQUE AUTO_INCREMENT NOT NULL,
+        name VARCHAR(256) NOT NULL,
+        PRIMARY KEY (id)
 );
 
-INSERT INTO `tyrell_corp.nexus6` (`name`) VALUES ("El Houssain");
+INSERT INTO tyrell_corp.nexus6 (name) VALUES ("ElHoussain");
+
+GRANT SELECT ON tyrell_corp.nexus6 TO 'holberton_user'@'localhost';
 
