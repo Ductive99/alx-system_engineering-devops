4aec8fd863f3265c222f325c6797383f16c8169c
El Houssain Souhail <sou.ductive@gmail.com>
2024-02-19 22:03:22 +0100

    Debugging

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     finished =  [t.get("title") for t in todos if t.get("finished") is True]
 
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+        user.get("name"), len(finished), len(todos)))
+    [print("\t {}".format(c)) for c in finished]
