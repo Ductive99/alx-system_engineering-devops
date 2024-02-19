0bc40423d75ca3a2172dd0b8bf0a327a0b2fda0f
El Houssain Souhail <sou.ductive@gmail.com>
2024-02-19 21:58:01 +0100

    Improved styling

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,11 +4,12 @@ Returns information about employee TODO list progress
 """
 import requests
 import sys
+
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
     finished = [t.get('title') for t in todos if t.get('completed') is True]
     print("Employee {} is done with tasks({}/{}):".format(
-          employee.get("name"), len(finished), len(todos)))
+          user.get("name"), len(finished), len(todos)))
     [print("\t {}".format(c)) for c in finished]
