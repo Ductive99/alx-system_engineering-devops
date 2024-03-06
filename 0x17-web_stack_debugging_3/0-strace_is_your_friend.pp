ac2e93972844de5dfb099b9d4e8029a1ef04f375
El Houssain Souhail <sou.ductive@gmail.com>
2024-03-06 15:05:49 +0100

    Added puppet code to fix typo

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes typo in wordpress, resolves 500 Apache error
+
+exec { 'fix-typo':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
